7369642dedb6451eefce49da85281b8505a5887d
deepblue <haol@vvipsoft.com>
2019-07-17 15:57:39 +0800

    deepblue

--- src/app/business-modules/components/security/fac/fac.component.ts
+++ src/app/business-modules/components/security/fac/fac.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd';
+import { StaffSercice } from 'src/app/services/common/staff-service';
+import { DictionarySercice } from 'src/app/services/common/dictionary.service';
+import { ServiceDepartService } from 'src/app/services/unit/servicedepart.service';
+import { FacSercice } from 'src/app/services/unit/fac.service';
+import { FacSecuritySercice } from 'src/app/services/security/fac.service';
 
 @Component({
   selector: 'app-fac',
@@ -7,9 +14,115 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacComponent implements OnInit {
 
-  constructor() { }
+  dictionary: any = {};
+  staffObj: any = {};
+
+  pageIndex: any = 1;
+  totalCount: any;
+  pageSize: any = 10;
+
+  dataSet: any = [];
+
+  name: any = "";
+
+  equipDepartIds: any = [];
+
+  equipDepartList: any = [];
+
+  serviceDepartIds: any = [];
+
+  serviceDepartList: any = [];
+
+  facIds: any = [];
+
+  facList: any = [];
+
+
+  constructor(private router: Router,
+    private msg: NzMessageService, private facSecuritySercice: FacSecuritySercice, private dictionarySercice: DictionarySercice,
+    private staffSercice: StaffSercice, private serviceDepartService: ServiceDepartService, private facSercice: FacSercice) { }
 
   ngOnInit() {
+
+    this.dictionary = this.dictionarySercice.getAllConfig();
+    this.staffObj = this.staffSercice.getStaffObj();
+
+    this.search();
+
+    this.serviceDepartService.getAllDepartService().subscribe((res) => {
+      if (res.code == 200) {
+        this.serviceDepartList = [];
+        res.msg.forEach(element => {
+          this.serviceDepartList.push({
+            id: element.id,
+            name: element.name
+          });
+        });
+      }
+    })
+
+
+    // this.facSercice.getAllDepartService().subscribe((res) => {
+    //   if (res.code == 200) {
+    //     this.serviceDepartList = [];
+    //     res.msg.forEach(element => {
+    //       this.serviceDepartList.push({
+    //         id: element.id,
+    //         name: element.name
+    //       });
+    //     });
+    //   }
+    // })
+
   }
 
+  search() {
+    var option = {
+      pageNo: this.pageIndex,
+      pageSize: this.pageSize,
+      conditions: []
+    }
+
+    if (this.name) {
+      option.conditions.push({ key: "name", value: this.name })
+    }
+
+    // if (this.groupIds.length > 0) {
+    //   option.conditions.push({ key: "groupIds", value: this.groupIds })
+    // }
+
+    this.facSecuritySercice.getFacSecurityList(option).subscribe(
+      (data) => {
+        this.dataSet = data.msg.currentList;
+        this.totalCount = data.msg.recordCount;
+      }
+    );
+  }
+
+  reset() {
+    this.name = "";
+    //this.groupIds = [];
+  }
+
+  add() {
+    this.router.navigate(['/monitor/check/add']);
+  }
+
+  show(item, flag) {
+    this.router.navigate(['/monitor/check/add'], { queryParams: { id: item.id, flag: flag } });
+  }
+
+  delete(item) {
+
+    this.facSecuritySercice.deleteFacSecurityById(item.id).subscribe((res) => {
+
+      if (res.code == 200) {
+        this.msg.create("success", "删除成功");
+        this.search();
+      } else {
+        this.msg.create("error", "删除失败");
+      }
+    })
+
+  }
 }
