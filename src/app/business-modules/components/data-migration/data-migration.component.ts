9731fdb16751d2e888733e3a93d6dd715f509854
yuhangc1030 <34096360+yuhangc1030@users.noreply.github.com>
2019-09-01 00:14:01 +0800

    数据迁移

--- src/app/business-modules/components/data-migration/data-migration.component.ts
+++ src/app/business-modules/components/data-migration/data-migration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NzMessageService } from 'ng-zorro-antd';
+import { NzMessageService, UploadXHRArgs } from 'ng-zorro-antd';
+import { HttpRequest, HttpClient, HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-data-migration',
@@ -17,7 +18,9 @@ export class DataMigrationComponent implements OnInit {
   ]
   types: any = [];
 
-  constructor(private msg: NzMessageService) { }
+  uploadUrl: any = AppConfig.serviceAddress + "/dataMigration/importData";
+
+  constructor(private msg: NzMessageService,private http: HttpClient) { }
 
   ngOnInit() {
   }
@@ -32,6 +35,41 @@ export class DataMigrationComponent implements OnInit {
     let str = this.types.join(",");
     window.open( AppConfig.serviceAddress+"/dataMigration/exportData?type="+str, "_blank");
 
+  }
+
+  customReq = (item: UploadXHRArgs) => {
+
+    var that = this;
+    // 构建一个 FormData 对象，用于存储文件或其他参数
+    const formData = new FormData();
+    // tslint:disable-next-line:no-any
+    formData.append('file', item.file as any);
+    formData.append('filename', item.file.name);
+
+    const req = new HttpRequest('POST', item.action, formData, {
+      reportProgress: true,
+      withCredentials: false
+    });
+    // 始终返回一个 `Subscription` 对象，nz-upload 会在适当时机自动取消订阅
+    return this.http.request(req).subscribe((event: any) => {
+      if (event.type === HttpEventType.UploadProgress) {
+        if (event.total > 0) {
+          // tslint:disable-next-line:no-any
+          (event as any).percent = event.loaded / event.total * 100;
+        }
+        // 处理上传进度条，必须指定 `percent` 属性来表示进度
+        item.onProgress(event, item.file);
+      } else if (event instanceof HttpResponse) {
+
+        // 处理成功
+        item.onSuccess(event.body, item.file, event);
+        that.msg.create("success", "导入成功");
+      }
+    }, (err) => {
+      // 处理失败
+      item.onError(err, item.file);
+      that.msg.create("error", "导入失败");
+    });
   }
 
 }
