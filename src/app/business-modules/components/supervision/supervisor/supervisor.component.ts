8782d72c2e99394ab310e5f9f0a63e568039c5c3
yuhangc1030 <34096360+yuhangc1030@users.noreply.github.com>
2019-09-07 21:49:14 +0800

    导入核安全监督员

--- src/app/business-modules/components/supervision/supervisor/supervisor.component.ts
+++ src/app/business-modules/components/supervision/supervisor/supervisor.component.ts
@@ -36,6 +36,8 @@ export class SupervisorComponent implements OnInit {
 
     selectId: any = "";
 
+    uploadUrl: any = AppConfig.serviceAddress + "/Supervisor/importSupervisor";
+
     constructor(private supervisionSercice: SupervisionSercice, private http: HttpClient, private router: Router,
         private msg: NzMessageService, private dictionarySercice: DictionarySercice,
         private staffSercice: StaffSercice, private utilitiesSercice: UtilitiesSercice) { }
@@ -44,6 +46,8 @@ export class SupervisorComponent implements OnInit {
 
         this.dictionary = this.dictionarySercice.getAllConfig();
         this.staffObj = this.staffSercice.getStaffObj();
+
+        this.uploadUrl = this.utilitiesSercice.wrapUrl(this.uploadUrl);
 
         this.search();
     }
@@ -81,12 +85,47 @@ export class SupervisorComponent implements OnInit {
         }
 
         let url = AppConfig.serviceAddress + "/Supervisor/exportSupervisor?name=" + this.name
-            + "&orgName=" + this.orgName + "&start_date=" +encodeURIComponent(start_date)+"&end_date="+encodeURIComponent(end_date)
-            +"&typeIds="+this.typeIds;
+            + "&orgName=" + this.orgName + "&start_date=" + encodeURIComponent(start_date) + "&end_date=" + encodeURIComponent(end_date)
+            + "&typeIds=" + this.typeIds;
 
         url = this.utilitiesSercice.wrapUrl(url);
         window.open(url, "_blank");
 
+    }
+
+    customReq = (item: UploadXHRArgs) => {
+
+        var that = this;
+        // 构建一个 FormData 对象，用于存储文件或其他参数
+        const formData = new FormData();
+        // tslint:disable-next-line:no-any
+        formData.append('file', item.file as any);
+        formData.append('filename', item.file.name);
+
+        const req = new HttpRequest('POST', item.action, formData, {
+            reportProgress: true,
+            withCredentials: false
+        });
+        // 始终返回一个 `Subscription` 对象，nz-upload 会在适当时机自动取消订阅
+        return this.http.request(req).subscribe((event: any) => {
+            if (event.type === HttpEventType.UploadProgress) {
+                if (event.total > 0) {
+                    // tslint:disable-next-line:no-any
+                    (event as any).percent = event.loaded / event.total * 100;
+                }
+                // 处理上传进度条，必须指定 `percent` 属性来表示进度
+                item.onProgress(event, item.file);
+            } else if (event instanceof HttpResponse) {
+
+                // 处理成功
+                item.onSuccess(event.body, item.file, event);
+                that.msg.create("success", "导入成功");
+            }
+        }, (err) => {
+            // 处理失败
+            item.onError(err, item.file);
+            that.msg.create("error", "导入失败");
+        });
     }
 
     search() {
