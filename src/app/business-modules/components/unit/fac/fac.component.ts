d74a1c2e494a210f85703a06e3c0cf2d6c42fe6e
chstyyzq <idealks@163.com>
2019-09-26 22:06:40 +0800

    添加导入功能
    添加导入功能

--- src/app/business-modules/components/unit/fac/fac.component.ts
+++ src/app/business-modules/components/unit/fac/fac.component.ts
@@ -13,8 +13,7 @@ import { UtilitiesSercice } from 'src/app/services/common/utilities.services';
   styleUrls: ['./fac.component.scss']
 })
 export class FacComponent implements OnInit {
-
-  @Input() isSearchShow = "0";
+  @Input() isSearchShow = '0';
 
   dictionary: any = {};
   staffObj: any = {};
@@ -26,36 +25,42 @@ export class FacComponent implements OnInit {
   dataSet: any = [];
   ServiceDepartList: any = [];
 
-  name: any = "";
-  code: any = "";
-  serviceDepart: any = "";
-  build_start_year: any = "";
-  build_end_year: any = "";
+  name: any = '';
+  code: any = '';
+  serviceDepart: any = '';
+  build_start_year: any = '';
+  build_end_year: any = '';
   supervisionCategoryIds: any = [];
   typeIds: any = [];
   statusIds: any = [];
   reviewStatusIds: any = [];
   permitSituationIds: any = [];
-  is_earthquake: any = "";
-  is_flood: any = "";
+  is_earthquake: any = '';
+  is_flood: any = '';
 
-  selectId: any = "";
+  selectId: any = '';
   canManage: any = false;
 
-  uploadUrl: any = AppConfig.serviceAddress + "/fac/importFac";
+  uploadUrl: any = AppConfig.serviceAddress + '/fac/importData';
 
-  constructor(private router: Router,
-    private msg: NzMessageService, private facSercice: FacSercice, private dictionarySercice: DictionarySercice,
-    private staffSercice: StaffSercice, private serviceDepartService: ServiceDepartService, private utilitiesSercice: UtilitiesSercice) { }
+  constructor(
+    private router: Router,
+    private msg: NzMessageService,
+    private facSercice: FacSercice,
+    private dictionarySercice: DictionarySercice,
+    private staffSercice: StaffSercice,
+    private serviceDepartService: ServiceDepartService,
+    private utilitiesSercice: UtilitiesSercice
+  ) {}
 
   ngOnInit() {
-
     this.dictionary = this.dictionarySercice.getAllConfig();
     this.staffObj = this.staffSercice.getStaffObj();
 
-    this.canManage = this.utilitiesSercice.checkPermission("group:manage");
+    this.uploadUrl = this.utilitiesSercice.wrapUrl(this.uploadUrl);
+    this.canManage = this.utilitiesSercice.checkPermission('group:manage');
 
-    this.serviceDepartService.getAllDepartService().subscribe((res) => {
+    this.serviceDepartService.getAllDepartService().subscribe(res => {
       if (res.code == 200) {
         this.ServiceDepartList = [];
         res.msg.forEach(element => {
@@ -65,7 +70,7 @@ export class FacComponent implements OnInit {
           });
         });
       }
-    })
+    });
 
     this.search();
   }
@@ -75,79 +80,97 @@ export class FacComponent implements OnInit {
       pageNo: this.pageIndex,
       pageSize: this.pageSize,
       conditions: []
-    }
+    };
 
     if (this.name) {
-      option.conditions.push({ key: "name", value: this.name })
+      option.conditions.push({ key: 'name', value: this.name });
     }
 
     if (this.code) {
-      option.conditions.push({ key: "code", value: this.code })
+      option.conditions.push({ key: 'code', value: this.code });
     }
 
     if (this.serviceDepart) {
-      option.conditions.push({ key: "serviceDepart", value: this.serviceDepart })
+      option.conditions.push({
+        key: 'serviceDepart',
+        value: this.serviceDepart
+      });
     }
 
     if (this.build_start_year) {
-      option.conditions.push({ key: "build_start_year", value: this.build_start_year })
+      option.conditions.push({
+        key: 'build_start_year',
+        value: this.build_start_year
+      });
     }
 
     if (this.build_end_year) {
-      option.conditions.push({ key: "build_end_year", value: this.build_end_year })
+      option.conditions.push({
+        key: 'build_end_year',
+        value: this.build_end_year
+      });
     }
 
-
     if (this.supervisionCategoryIds && this.supervisionCategoryIds.length > 0) {
-      option.conditions.push({ key: "supervisionCategoryIds", value: [this.supervisionCategoryIds] })
+      option.conditions.push({
+        key: 'supervisionCategoryIds',
+        value: [this.supervisionCategoryIds]
+      });
     }
 
     if (this.typeIds && this.typeIds.length > 0) {
-      option.conditions.push({ key: "typeIds", value: [this.typeIds] })
+      option.conditions.push({ key: 'typeIds', value: [this.typeIds] });
     }
 
     if (this.statusIds && this.statusIds.length > 0) {
-      option.conditions.push({ key: "statusIds", value: [this.statusIds] })
+      option.conditions.push({ key: 'statusIds', value: [this.statusIds] });
     }
 
     if (this.reviewStatusIds && this.reviewStatusIds.length > 0) {
-      option.conditions.push({ key: "reviewStatusIds", value: [this.reviewStatusIds] })
+      option.conditions.push({
+        key: 'reviewStatusIds',
+        value: [this.reviewStatusIds]
+      });
     }
 
     if (this.permitSituationIds && this.permitSituationIds.length > 0) {
-      option.conditions.push({ key: "permitSituationIds", value: [this.permitSituationIds] })
+      option.conditions.push({
+        key: 'permitSituationIds',
+        value: [this.permitSituationIds]
+      });
     }
 
     if (this.is_earthquake) {
-      option.conditions.push({ key: "is_earthquake", value: this.is_earthquake })
+      option.conditions.push({
+        key: 'is_earthquake',
+        value: this.is_earthquake
+      });
     }
 
     if (this.is_flood) {
-      option.conditions.push({ key: "is_flood", value: this.is_flood })
+      option.conditions.push({ key: 'is_flood', value: this.is_flood });
     }
 
-    this.facSercice.getFacList(option).subscribe(
-      (data) => {
-        this.dataSet = data.msg.currentList;
-        this.totalCount = data.msg.recordCount;
-      }
-    );
+    this.facSercice.getFacList(option).subscribe(data => {
+      this.dataSet = data.msg.currentList;
+      this.totalCount = data.msg.recordCount;
+    });
   }
 
   reset() {
-    this.name = "";
-    this.code = "";
-    this.serviceDepart = "";
-    this.build_start_year = "";
-    this.build_end_year = "";
+    this.name = '';
+    this.code = '';
+    this.serviceDepart = '';
+    this.build_start_year = '';
+    this.build_end_year = '';
     this.supervisionCategoryIds = [];
     this.typeIds = [];
     this.statusIds = [];
     this.reviewStatusIds = [];
     this.permitSituationIds = [];
-    this.is_earthquake = "";
-    this.is_flood = "";
-    this.selectId = "";
+    this.is_earthquake = '';
+    this.is_flood = '';
+    this.selectId = '';
   }
 
   add() {
@@ -159,39 +182,41 @@ export class FacComponent implements OnInit {
   // }
 
   show(item) {
-
-    if (this.isSearchShow == "0") {
-      this.router.navigate(['/unit/fac/add'], { queryParams: { id: item.id, isShow: true } });
+    if (this.isSearchShow == '0') {
+      this.router.navigate(['/unit/fac/add'], {
+        queryParams: { id: item.id, isShow: true }
+      });
     } else {
-      this.router.navigate(['/searchShow/integratedAuery/facSearch'], { queryParams: { id: item.id } });
+      this.router.navigate(['/searchShow/integratedAuery/facSearch'], {
+        queryParams: { id: item.id }
+      });
     }
   }
 
   modify() {
     if (this.selectId) {
-      this.router.navigate(['/unit/fac/add'], { queryParams: { id: this.selectId, isShow: false } });
+      this.router.navigate(['/unit/fac/add'], {
+        queryParams: { id: this.selectId, isShow: false }
+      });
     } else {
-      this.msg.create("warning", "请选择修改项");
+      this.msg.create('warning', '请选择修改项');
     }
   }
 
   delete() {
-
     if (this.selectId) {
-      this.facSercice.deleteFacById(this.selectId).subscribe((res) => {
-
+      this.facSercice.deleteFacById(this.selectId).subscribe(res => {
         if (res.code == 200) {
-          this.msg.create("success", res.msg);
+          this.msg.create('success', res.msg);
           this.search();
         } else if (res.code == 500) {
-          this.msg.create("warning", res.msg);
+          this.msg.create('warning', res.msg);
         } else {
-          this.msg.create("error", res.msg);
+          this.msg.create('error', res.msg);
         }
-      })
-
+      });
     } else {
-      this.msg.create("warning", "请选择删除项");
+      this.msg.create('warning', '请选择删除项');
     }
   }
 
@@ -211,16 +236,34 @@ export class FacComponent implements OnInit {
   }
 
   exportData() {
-
-
-    let url = AppConfig.serviceAddress + "/fac/exportFac?name=" + this.name
-      + "&code=" + this.code + "&serviceDepart=" + this.serviceDepart + "&build_start_year=" + encodeURIComponent(this.build_start_year)
-      + "&build_end_year=" + encodeURIComponent(this.build_end_year) + "&supervisionCategoryIds=" + this.supervisionCategoryIds
-      + "&typeIds=" + this.typeIds + "&statusIds=" + this.statusIds + "&reviewStatusIds=" + this.reviewStatusIds
-      + "&permitSituationIds=" + this.permitSituationIds + "&is_earthquake=" + this.is_earthquake
-      + "&is_flood=" + this.is_flood;
+    let url =
+      AppConfig.serviceAddress +
+      '/fac/exportFac?name=' +
+      this.name +
+      '&code=' +
+      this.code +
+      '&serviceDepart=' +
+      this.serviceDepart +
+      '&build_start_year=' +
+      encodeURIComponent(this.build_start_year) +
+      '&build_end_year=' +
+      encodeURIComponent(this.build_end_year) +
+      '&supervisionCategoryIds=' +
+      this.supervisionCategoryIds +
+      '&typeIds=' +
+      this.typeIds +
+      '&statusIds=' +
+      this.statusIds +
+      '&reviewStatusIds=' +
+      this.reviewStatusIds +
+      '&permitSituationIds=' +
+      this.permitSituationIds +
+      '&is_earthquake=' +
+      this.is_earthquake +
+      '&is_flood=' +
+      this.is_flood;
 
     url = this.utilitiesSercice.wrapUrl(url);
-    window.open(url, "_blank");
+    window.open(url, '_blank');
   }
 }
--- src/app/business-modules/components/unit/umineplace/umineplace.component.ts
+++ src/app/business-modules/components/unit/umineplace/umineplace.component.ts
@@ -5,6 +5,7 @@ import { UmineService } from 'src/app/services/unit/umine.service';
 import { DictionarySercice } from 'src/app/services/common/dictionary.service';
 import { StaffSercice } from 'src/app/services/common/staff-service';
 import { UminePlaceService } from 'src/app/services/unit/umineplace.service';
+import { UtilitiesSercice } from 'src/app/services/common/utilities.services';
 
 @Component({
   selector: 'app-umineplace',
@@ -12,8 +13,7 @@ import { UminePlaceService } from 'src/app/services/unit/umineplace.service';
   styleUrls: ['./umineplace.component.scss']
 })
 export class UmineplaceComponent implements OnInit {
-
-  @Input() isSearchShow = "0";
+  @Input() isSearchShow = '0';
 
   dictionary: any = {};
   staffObj: any = {};
@@ -24,30 +24,34 @@ export class UmineplaceComponent implements OnInit {
 
   dataSet: any = [];
 
-  name: any = "";
-  umineName: any = "";
-  build_start_year: any = "";
-  build_end_year: any = "";
+  name: any = '';
+  umineName: any = '';
+  build_start_year: any = '';
+  build_end_year: any = '';
   levelIds: any = [];
   statusIds: any = [];
   reviewStatusIds: any = [];
   permitSituationIds: any = [];
-  have_monitor: any = "";
+  have_monitor: any = '';
 
-  selectId: any = "";
+  selectId: any = '';
+  uploadUrl: any = AppConfig.serviceAddress + '/umineplace/importData';
 
-  constructor(private router: Router,
-    private msg: NzMessageService, private umineSercice: UmineService, private dictionarySercice: DictionarySercice,
-    private staffSercice: StaffSercice, private uminePlaceService: UminePlaceService) { }
+  constructor(
+    private router: Router,
+    private msg: NzMessageService,
+    private umineSercice: UmineService,
+    private dictionarySercice: DictionarySercice,
+    private staffSercice: StaffSercice,
+    private uminePlaceService: UminePlaceService,
+    private utilitiesSercice: UtilitiesSercice
+  ) {}
 
   ngOnInit() {
-
     this.dictionary = this.dictionarySercice.getAllConfig();
     this.staffObj = this.staffSercice.getStaffObj();
-
+    this.uploadUrl = this.utilitiesSercice.wrapUrl(this.uploadUrl);
     this.search();
-
-
   }
 
   search() {
@@ -55,63 +59,73 @@ export class UmineplaceComponent implements OnInit {
       pageNo: this.pageIndex,
       pageSize: this.pageSize,
       conditions: []
-    }
+    };
 
     if (this.name) {
-      option.conditions.push({ key: "name", value: this.name })
+      option.conditions.push({ key: 'name', value: this.name });
     }
 
     if (this.umineName) {
-      option.conditions.push({ key: "umineName", value: this.umineName })
+      option.conditions.push({ key: 'umineName', value: this.umineName });
     }
 
     if (this.build_start_year) {
-      option.conditions.push({ key: "build_start_year", value: this.build_start_year })
+      option.conditions.push({
+        key: 'build_start_year',
+        value: this.build_start_year
+      });
     }
 
     if (this.build_end_year) {
-      option.conditions.push({ key: "build_end_year", value: this.build_end_year })
+      option.conditions.push({
+        key: 'build_end_year',
+        value: this.build_end_year
+      });
     }
 
     if (this.levelIds.length > 0) {
-      option.conditions.push({ key: "levelIds", value: [this.levelIds] })
+      option.conditions.push({ key: 'levelIds', value: [this.levelIds] });
     }
 
     if (this.statusIds.length > 0) {
-      option.conditions.push({ key: "statusIds", value: [this.statusIds] })
+      option.conditions.push({ key: 'statusIds', value: [this.statusIds] });
     }
 
     if (this.reviewStatusIds.length > 0) {
-      option.conditions.push({ key: "reviewStatusIds", value: [this.reviewStatusIds] })
+      option.conditions.push({
+        key: 'reviewStatusIds',
+        value: [this.reviewStatusIds]
+      });
     }
 
     if (this.permitSituationIds.length > 0) {
-      option.conditions.push({ key: "permitSituationIds", value: [this.permitSituationIds] })
+      option.conditions.push({
+        key: 'permitSituationIds',
+        value: [this.permitSituationIds]
+      });
     }
 
     if (this.have_monitor) {
-      option.conditions.push({ key: "have_monitor", value: this.have_monitor })
+      option.conditions.push({ key: 'have_monitor', value: this.have_monitor });
     }
 
-    this.uminePlaceService.getUminePlaceList(option).subscribe(
-      (data) => {
-        this.dataSet = data.msg.currentList;
-        this.totalCount = data.msg.recordCount;
-      }
-    );
+    this.uminePlaceService.getUminePlaceList(option).subscribe(data => {
+      this.dataSet = data.msg.currentList;
+      this.totalCount = data.msg.recordCount;
+    });
   }
 
   reset() {
-    this.name = "";
-    this.umineName = "";
-    this.build_start_year = "";
-    this.build_end_year = "";
+    this.name = '';
+    this.umineName = '';
+    this.build_start_year = '';
+    this.build_end_year = '';
     this.levelIds = [];
     this.statusIds = [];
     this.reviewStatusIds = [];
     this.permitSituationIds = [];
-    this.have_monitor = "";
-    this.selectId = "";
+    this.have_monitor = '';
+    this.selectId = '';
   }
 
   add() {
@@ -119,45 +133,49 @@ export class UmineplaceComponent implements OnInit {
   }
 
   childmanage(item) {
-    this.router.navigate(['/unit/umineplace/childmanage'], { queryParams: { id: item.id } });
+    this.router.navigate(['/unit/umineplace/childmanage'], {
+      queryParams: { id: item.id }
+    });
   }
 
   show(item) {
-
     if (this.isSearchShow == '0') {
-      this.router.navigate(['/unit/umineplace/add'], { queryParams: { id: item.id, isShow: true } });
+      this.router.navigate(['/unit/umineplace/add'], {
+        queryParams: { id: item.id, isShow: true }
+      });
     } else {
-      this.router.navigate(['/searchShow/integratedAuery/umineplaceSearch'], { queryParams: { id: item.id } });
+      this.router.navigate(['/searchShow/integratedAuery/umineplaceSearch'], {
+        queryParams: { id: item.id }
+      });
     }
-
   }
 
   modify() {
     if (this.selectId) {
-      this.router.navigate(['/unit/umineplace/add'], { queryParams: { id: this.selectId, isShow: false } });
+      this.router.navigate(['/unit/umineplace/add'], {
+        queryParams: { id: this.selectId, isShow: false }
+      });
     } else {
-      this.msg.create("warning", "请选择修改项");
+      this.msg.create('warning', '请选择修改项');
     }
   }
 
   delete() {
-
     if (this.selectId) {
-
-      this.uminePlaceService.deleteUminePlaceById(this.selectId).subscribe((res) => {
-
-        if (res.code == 200) {
-          this.msg.create("success", res.msg);
-          this.search();
-        } else if (res.code == 500) {
-          this.msg.create("warning", res.msg);
-        } else {
-          this.msg.create("error", res.msg);
-        }
-      })
-
+      this.uminePlaceService
+        .deleteUminePlaceById(this.selectId)
+        .subscribe(res => {
+          if (res.code == 200) {
+            this.msg.create('success', res.msg);
+            this.search();
+          } else if (res.code == 500) {
+            this.msg.create('warning', res.msg);
+          } else {
+            this.msg.create('error', res.msg);
+          }
+        });
     } else {
-      this.msg.create("warning", "请选择删除项");
+      this.msg.create('warning', '请选择删除项');
     }
   }
 
@@ -175,5 +193,4 @@ export class UmineplaceComponent implements OnInit {
     this.pageIndex = 1;
     this.search();
   }
-
 }
